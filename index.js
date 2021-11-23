e52cc7413b0592924c89db2352c53a67c3904f20
Sunil <snilavarath@gmail.com>
2021-11-23 12:56:58 -0500

    commit tree ff79166520c4d6b75016f3a15eb5d8a6d82a5dba

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
 
-let mondayWork = function(activity="go to the office") {
+const mondayWork = function (activity='go to the office') {
   return `This Monday, I will ${activity}.`
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
-  }
-}
+const wrapAdjective = function (par1 = "*") {
+    return function (par2="special") {    
+           return `You are ${par1}${par2}${par1}!`      
+    };
+ }
